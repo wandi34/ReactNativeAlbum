76b6f81cfac7fdfb6c8df4e866ff1bb096341ab9
wandi34 <info@andreas-winter.eu>
2017-06-19 22:13:30 +0200

    Initialize

---
+++ index.android.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a Component
+const App = () => {
+  return (
+    <View>
+      <Header headerText={'Albums'}/>
+      <AlbumList />
+    </View>
+  )
+}
+
+
+
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#62a3cf',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 20 },
+    shadowOpacity: 0.8,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+}
+
+export default Header;
